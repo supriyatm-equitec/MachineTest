835ed3ddec206bcdd25ca70156e564df97ad2d98
supriyatm-equitec <supriyatm.equitec@gmail.com>
2024-12-01 07:09:08 -0800

    Done machine test

---
+++ MachinTest/MachinTest/Models/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MachinTest.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        public AppDbContext() : base("name=DefaultConnection")
+        {
+        }
+    
+    }
+}
---
+++ MachinTest/MachinTest/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MachinTest.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
